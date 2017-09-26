9e9442e108f08317427dc95de15a8bbd98cca1f6
Claison <30608657+Claison@users.noreply.github.com>
2017-09-25 21:29:43 -0300

    trabalho 2 lpc
    Correção admin

--- g1_lpc/calendario/agenda/admin.py
+++ g1_lpc/calendario/agenda/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from agenda.models import Agenda,Compromisso
+from agenda.models import Agenda,Compromisso,UsuarioAgenda,UsuarioCompromisso
 
 
 admin.site.register(Agenda)
 admin.site.register(Compromisso)
+admin.site.register(UsuarioAgenda)
+admin.site.register(UsuarioCompromisso)
